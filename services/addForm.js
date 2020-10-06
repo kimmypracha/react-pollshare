648c424d941f2017b21282137a0ac39fda94ae34
kimmypracha <pracha.promtaow@gmail.com>
2020-10-06 17:14:35 +0700

    add voted number into account

--- services/addForm.js
+++ services/addForm.js
@@ -3,7 +3,8 @@ async function addForm(value,username){
     value = {
         ...value,
         createdAt : Date.now(),
-        createdBy : username
+        createdBy : username,
+        voted: 0
     }
     console.log(JSON.stringify(value,null,2));
     const res = await db.collection('poll').add(value);
