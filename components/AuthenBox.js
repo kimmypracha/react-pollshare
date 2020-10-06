9d609108cccfe633fb8aa3a3244be3c0f9c1c879
kimmypracha <pracha.promtaow@gmail.com>
2020-10-06 17:12:31 +0700

    make signup temporarily disable

--- components/AuthenBox.js
+++ components/AuthenBox.js
@@ -3,17 +3,17 @@ import Login from './Login';
 import Signup from './Signup';
 function AuthenBox(){
         const [authenType, setAuthenType] = useState('login');
-        var temporary_jsx = (<Signup/>);
-        if(authenType == 'login'){
+        var temporary_jsx = (<h1>Sorry, this feature is not available right now.</h1>);
+        if(authenType === 'login'){
             temporary_jsx = <Login/>;
-        }else{
+        }else if(authenType === 'signup'){
             temporary_jsx = (<Signup/>);
         }
         return (<main>
             {temporary_jsx}
           <footer>
                 <a value='login' onClick={e => setAuthenType('login')}>Login</a>
-                <a value='signup' onClick={e => setAuthenType('signup')}>Sign up</a>
+                <a value='signup' onClick={e => setAuthenType('nosignup')} disabled>Sign up</a>
             </footer>
         </main>)
 }
